f147e9f73adcbb25b8b84b10b1fafd8f9ceef30a
RichPaschburg11 <rpaschburg@gmail.com>
2015-07-20 00:13:57 -0700

    File save with JSON return from openweather
    Can get forecast for pre-defined city

--- app/src/main/java/com/richpaschburg/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/richpaschburg/sunshine/app/ForecastFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -27,6 +30,28 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -77,7 +102,7 @@ public class ForecastFragment extends Fragment {
 
             try
             {
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?a=95051&mode=json&units=metric&cnt=7");
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=Santa%20Clara,us&mode=json&units=metric&cnt=7");
 
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
@@ -102,6 +127,8 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
+
+                Log.e(LOG_TAG, "Forecast JSON String: " + forecastJsonStr);
             }
             catch (IOException e)
             {
